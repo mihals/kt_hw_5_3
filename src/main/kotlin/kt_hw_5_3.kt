351d312895c995eb9698166bbd5a773adcc087a4
mihals <mihalsnes@gmail.com>
2022-03-28 18:21:33 +0300

    New branch for hw 6_1

--- src/main/kotlin/kt_hw_5_3.kt
+++ src/main/kotlin/kt_hw_5_3.kt
@@ -10,21 +10,25 @@ data class Post(
     val replyOwnerId: Long = -1L,
     val replyPostId: Long = -1L,
     val friendsOnly: Boolean = false,
-    val comments: Boolean = false,
+    val comments: Any? = null,
     val copyright: String = "",
     val likes: Long = -1L,
-    val reposts: Long = -1L,
-    val views: Long = -1L,
+    val reposts: Any? = null,
+    val views: Any? = null,
     val postType: String = "",
+    val postSource: Any? = null,
+    val geo: Any? = null,
     val signerId: Long = -1L,
+    val copyHistory: Any? = null,
     val canPin: Boolean = false,
     val canDelete: Boolean = false,
     val canEdit: Boolean = false,
     val isPinned: Boolean = false,
     val markedAsAdds: Boolean = false,
     val isFavorite: Boolean = false,
-    val postponedId: Long = -1L
+    val postponedId: Long = -1L,
 )
+
 
 class WallService {
     private var posts = emptyArray<Post>()
